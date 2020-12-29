7587eaf019457bd13c4b6b795cae192ebcdc4296
Roberto Valderrama <55214007+RobbieLopez@users.noreply.github.com>
2020-12-29 14:35:55 -0600

    Update rock_lml DB.sql

--- Base de datos/rock_lml DB.sql
+++ Base de datos/rock_lml DB.sql
@@ -4,53 +4,58 @@ create database rock_lml;
 use rock_lml;
 
 create table Artista(
-	idArtista int primary key not null,
-   	nombre varchar(40) null,
-    	apellidoPat varchar(40) null,
-    	apellidoMat varchar(40) null,
-    	genero varchar(40) null,
+	idArtista int auto_increment,
+    	nombre varchar(40),
+    	apellidoPat varchar(40),
+    	apellidoMat varchar(40),
+    	genero varchar(40),
     	ciudadNac varchar(20),
     	biografia mediumtext,
-    	sitioWeb varchar(100)
+    	sitioWeb varchar(100),
+    	primary key  (idArtista)
 );
 
 create table Album(
-	idAlbum int primary key not null,
+	idAlbum int auto_increment,
     	idArtista int not null,
     	nombreAlbum varchar(70),
     	anioLanzamiento date,
     	numPistas int(2),
     	estudio varchar(50),
     	imgPor longblob,
-    	imgContra longblob
+    	imgContra longblob,
+    	primary key (idAlbum)
 );
 
 create table Cancion(
-	idCancion int primary key not null,
+	idCancion int auto_increment,
     	idAlbum int not null,
     	nombreCancion varchar (70),
     	duracion time,
     	numPista int(2),
-    	url varchar (100)
+    	url varchar (100),
+    	primary key (idCancion)
 );
 
 create table companiaGrab(
-	idCompania int primary key not null,
+	idCompania int auto_increment,
 	idAlbum int not null,
     	nombreComp varchar(50),
     	direccion varchar(200),
     	sitioWeb varchar(100),
-    	numeroTel int(10)
+    	numeroTel int(10),
+    	primary key (idCompania)
 );
 
 create table grupoMusico(
-	idBanda int primary key not null,
+	idBanda int auto_increment,
 	idMusico int not null,
-    	nombreBanda varchar (50)
+    	nombreBanda varchar (50),
+    	primary key (idBanda)
 );
 
 create table musicos(
-	idMusico int primary key not null,
+	idMusico int auto_increment,
 	idArtista int not null,
     	idCancion int not null,
     	nombreMusico varchar(40),
@@ -59,14 +64,16 @@ create table musicos(
     	instrumento varchar(30),
     	ciudadNac varchar (30),
     	influenciado char(2),
-    	invitado char (2)
+    	invitado char (2),
+    	primary key (idMusico)
 );
 
 create table traductorCancion(
-	idTraductor int primary key not null,
+	idTraductor int auto_increment,
 	idCancion int not null,
     	nombre varchar(100),
-    	correo varchar (100)
+    	correo varchar (100),
+    	primary key (idTraductor)
 );
 
 ALTER TABLE Album ADD FOREIGN KEY(idArtista) REFERENCES Artista(idArtista);
