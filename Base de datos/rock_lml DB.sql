ad899c1140a2bc921c7b34be6b2e33dafbe3c14d
Roberto Valderrama <55214007+RobbieLopez@users.noreply.github.com>
2021-01-25 12:33:31 -0600

    Update rock_lml DB.sql

--- Base de datos/rock_lml DB.sql
+++ Base de datos/rock_lml DB.sql
@@ -4,82 +4,73 @@ create database rock_lml;
 use rock_lml;
 
 create table Artista(
-	idArtista int auto_increment,
-    	nombre varchar(40),
-    	apellidoPat varchar(40),
-    	apellidoMat varchar(40),
+    	nombreArtista varchar(40),
     	genero varchar(40),
     	ciudadNac varchar(20),
     	biografia mediumtext,
     	sitioWeb varchar(100),
-    	primary key  (idArtista)
-);
+    	primary key  (nombreArtista)
+)ENGINE=InnoDB DEFAULT CHARSET=utf8;
 
 create table Album(
-	idAlbum int auto_increment,
-    	idArtista int not null,
-    	nombreAlbum varchar(70),
+		nombreArtista varchar(40),
+    	Album varchar(70),
     	anioLanzamiento date,
     	numPistas int(2),
     	estudio varchar(50),
     	imgPor longblob,
     	imgContra longblob,
-    	primary key (idAlbum)
-);
+    	primary key (Album)
+)ENGINE=InnoDB DEFAULT CHARSET=utf8;
 
 create table Cancion(
-	idCancion int auto_increment,
-    	idAlbum int not null,
-    	nombreCancion varchar (70),
+		nombreArtista varchar(40),
+    	Album varchar(70),
+    	Cancion varchar (70),
     	duracion time,
     	numPista int(2),
     	url varchar (100),
-    	primary key (idCancion)
-);
+    	primary key (Cancion)
+)ENGINE=InnoDB DEFAULT CHARSET=utf8;
 
 create table companiaGrab(
-	idCompania int auto_increment,
-	idAlbum int not null,
+		Album varchar(70),
     	nombreComp varchar(50),
     	direccion varchar(200),
     	sitioWeb varchar(100),
     	numeroTel int(10),
-    	primary key (idCompania)
-);
+    	primary key (nombreComp)
+)ENGINE=InnoDB DEFAULT CHARSET=utf8;
 
 create table grupoMusico(
-	idBanda int auto_increment,
-	idMusico int not null,
+		nombreMusico varchar(40),
     	nombreBanda varchar (50),
-    	primary key (idBanda)
-);
+    	primary key (nombreBanda)
+)ENGINE=InnoDB DEFAULT CHARSET=utf8;
 
 create table musicos(
-	idMusico int auto_increment,
-	idArtista int not null,
-    	idCancion int not null,
+		nombreArtista varchar(40),
+    	Cancion varchar (70),
     	nombreMusico varchar(40),
-    	apellidoPat varchar(40),
-    	apellidoMat varchar(40),
     	instrumento varchar(30),
     	ciudadNac varchar (30),
     	influenciado char(2),
     	invitado char (2),
-    	primary key (idMusico)
-);
+    	primary key (nombreMusico)
+)ENGINE=InnoDB DEFAULT CHARSET=utf8;
 
 create table traductorCancion(
-	idTraductor int auto_increment,
-	idCancion int not null,
-    	nombre varchar(100),
+		Cancion varchar (70),
+    	nombreTraductor varchar(100),
     	correo varchar (100),
-    	primary key (idTraductor)
-);
+    	primary key (nombreTraductor)
+)ENGINE=InnoDB DEFAULT CHARSET=utf8;
 
-ALTER TABLE Album ADD FOREIGN KEY(idArtista) REFERENCES Artista(idArtista);
-ALTER TABLE Cancion ADD FOREIGN KEY(idAlbum) REFERENCES Album(idAlbum);
-ALTER TABLE companiaGrab ADD FOREIGN KEY(idAlbum) REFERENCES Album(idAlbum);
-ALTER TABLE grupoMusico ADD FOREIGN KEY(idMusico) REFERENCES musicos(idMusico);
-ALTER TABLE musicos ADD FOREIGN KEY(idArtista) REFERENCES Artista(idArtista);
-ALTER TABLE musicos ADD FOREIGN KEY(idCancion) REFERENCES Cancion(idCancion);
-ALTER TABLE traductorCancion ADD FOREIGN KEY(idCancion) REFERENCES Cancion(idCancion);
+ALTER TABLE Album ADD FOREIGN KEY(nombreArtista) REFERENCES Artista(nombreArtista);
+ALTER TABLE Cancion ADD FOREIGN KEY(Album) REFERENCES Album(Album);
+ALTER TABLE Cancion ADD FOREIGN KEY(nombreArtista) REFERENCES Artista(nombreArtista);
+ALTER TABLE companiaGrab ADD FOREIGN KEY(Album) REFERENCES Album(Album);
+ALTER TABLE grupoMusico ADD FOREIGN KEY(nombreMusico) REFERENCES musicos(nombreMusico);
+ALTER TABLE musicos ADD FOREIGN KEY(nombreArtista) REFERENCES Artista(nombreArtista);
+ALTER TABLE musicos ADD FOREIGN KEY(Cancion) REFERENCES Cancion(Cancion);
+ALTER TABLE traductorCancion ADD FOREIGN KEY(Cancion) REFERENCES Cancion(Cancion);
